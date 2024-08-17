8ca9ed6105b2608e228303aef3586054e92febe7
xovzd <144159255+2youb00@users.noreply.github.com>
2024-08-17 12:59:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 let stepCount = 0;
 let lastAcceleration = { x: 0, y: 0, z: 0 };
 let stepThreshold = 10;  // عتبة الكشف عن الخطوة
+let alertShown = false;
+
 if(window.DeviceMotionEvent){
-    alert("device is supported");
-    
-window.addEventListener('devicemotion', function(event) {
-    let currentAcceleration = event.acceleration;
-    
-    let deltaX = Math.abs(currentAcceleration.x - lastAcceleration.x);
-    let deltaY = Math.abs(currentAcceleration.y - lastAcceleration.y);
-    let deltaZ = Math.abs(currentAcceleration.z - lastAcceleration.z);
-
-    let totalAcceleration = deltaX + deltaY + deltaZ;
-
-    if (totalAcceleration > stepThreshold) {
-        stepCount++;
-        document.querySelector('.nbr-step span').innerHTML = stepCount;
+    if (!alertShown) {
+        alert("device is supported");
+        alertShown = true; // تحديد أن التنبيه قد ظهر
     }
 
-    lastAcceleration = currentAcceleration;
-});
+    window.addEventListener('devicemotion', function(event) {
+        let currentAcceleration = event.acceleration;
+        
+        let deltaX = Math.abs(currentAcceleration.x - lastAcceleration.x);
+        let deltaY = Math.abs(currentAcceleration.y - lastAcceleration.y);
+        let deltaZ = Math.abs(currentAcceleration.z - lastAcceleration.z);
+
+        let totalAcceleration = deltaX + deltaY + deltaZ;
+
+        if (totalAcceleration > stepThreshold) {
+            stepCount++;
+            document.querySelector('.nbr-step span').innerHTML = stepCount;
+        }
+
+        lastAcceleration = currentAcceleration;
+    });
 }
 else{
     alert("device is not supported");
